9ad47520e05ef4cfdaa7becc8f49051338232cf1
Martin Raquion <raquionearl@gmail.com>
2020-06-30 08:49:28 +0800

    initialize interface

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <form>
+          <input />
+          <button>Add Todo</button>
+        </form>
+        <div style={{ marginTop: "1rem" }}>
+          <div style={{ display: "flex" }}>
+            <div style={{ marginRight: 10 }}> - Todo 1</div>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        </div>
+      </header>
     </div>
   );
 }
